3edafb75d8d23cf300e2821059a4c2f431a38658
ryan <rshave@gmail.com>
2024-12-12 08:29:22 -0600

    more updates

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from flask import Flask, request, render_template, redirect, url_for, flash
-from twilio.rest import Client
-import os
 
 app = Flask(__name__)
 app.secret_key = 'yes'
@@ -15,32 +13,38 @@ twilio_number = ''
 def index():
     return render_template('index.html')
 
+
 @app.route('/admin')
 def admin():
     return render_template('admin.html')
+
 
 @app.route('/patient')
 def patient():
     return render_template('patient.html')
 
+
 @app.route('/ma')
 def medicalassistant():
     return render_template('medicalassistant.html')
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
 
+
 @app.route('/submit_contact', methods=['POST'])
 def submit_contact():
-    #retrieve form data
-    name =request.form.get('name')
-    email =request.form.get('email')
-    message =request.form.get('message')
+    # retrieve form data
+    name = request.form.get('name')
+    email = request.form.get('email')
+    message = request.form.get('message')
 
     if not name or not email or not message:
         flash('All fields required', 'error')
@@ -52,11 +56,13 @@ def submit_contact():
 
 @app.route('/confirmation')
 def confirmation():
-    slot =request.args.get('slot')
+    slot = request.args.get('slot')
     return render_template('confirmation.html', slot=slot)
+
 
 def send_sms(phone, slot):
     print('phone: {0} slot:{1}'.format(phone, slot))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
